fa8b0ff66f68e3012185a04c59a6ad5d0856cdc4
wljintao <wljintao@gmail.com>
2023-06-28 09:20:29 +0800

    Update site.config.ts
    修改notion配置ID

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '143097b5bc0f40e8a5f22a193d509376',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '做个梦科技',
+  domain: 'zuogemeng.cn',
+  author: '云峰',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '持续做正确的事',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
